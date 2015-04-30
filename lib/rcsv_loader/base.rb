81d79c77cadf25966ce8ca10c7204bccc581eda7
osadake212 <osadake.212@gmail.com>
2015-04-30 12:11:04 +0900

    modify the dependencies * delete class_macros.rb and loader.rb * add 'ActiveSupport' dependency in gemspec * modify base.rb and core.rb

--- lib/rcsv_loader/base.rb
+++ lib/rcsv_loader/base.rb
@@ -1,18 +1,11 @@
 require 'csv'
 require "rcsv_loader/version"
-require 'rcsv_loader/class_macros'
 require 'rcsv_loader/core'
-require 'rcsv_loader/loader'
-require 'rcsv_loader/where'
 
 module RCsvLoader
 
   class Base
-    extend ClassMacros
-    extend Loader
-
     include Core
-    include Where
   end
 
 end
